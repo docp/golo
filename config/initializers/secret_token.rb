161e068c7ab159eb6755040c6798c0e2b90abc3c
docp <paul_harmsen@web.de>
2010-11-04 23:17:39 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Golo::Application.config.secret_token = '935402b1e82c8eb0025c97db9280364af0a8fb9d66dbf8c5260af53c9f826371a9fac961becb361998001deff27b076be5c2373c76b770fe2d1cfd13df906c9e'
